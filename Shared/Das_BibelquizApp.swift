7e53a7667557c405379fc4ca9bae4cde873edb3d
Dominik Sigmund <dominik.sigmund@br.de>
2022-08-03 21:32:00 +0200

    Initial Commit

---
+++ Shared/Das_BibelquizApp.swift
@@ -0,0 +1,17 @@
+//
+//  Das_BibelquizApp.swift
+//  Shared
+//
+//  Created by Dominik Sigmund on 03.08.22.
+//
+
+import SwiftUI
+
+@main
+struct Das_BibelquizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
